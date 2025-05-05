7d09a73eea06951a1f84063e7a94f3e0cc2c47eb
phumudzo03 <tshivhulas.p3@gmail.com>
2025-05-05 09:40:01 +0200

    first commit

--- src/components/AdvertSection.js
+++ src/components/AdvertSection.js
@@ -6,13 +6,13 @@ const ads = [
   { title: 'Nengwis electronics', imageUrl: '/images/ad1.jpg' },
   
   { title: 'Nengwis electronics', imageUrl: '/images/ad4.jpg' },
-  { title: 'p3.dev- web service', imageUrl: '/images/ad5.jpg' },
+  { title: 'P3.dev- web service', imageUrl: '/images/ad5.jpg' },
 ];
 
 const AdvertSection = () => {
   return (
     <div className="advert-section">
-      <h2>Our Advertisers</h2>
+      <h2> Advertment</h2>
       <div className="advert-grid">
         {ads.map((ad, index) => (
           <div key={index} className="advert-card">
